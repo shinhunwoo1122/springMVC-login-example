48e429b1f0c43b43c1dfa6c383dfad5e1762015b
shinhunwoo <shinhunwoo1122@gmail.com>
2023-05-23 22:47:14 +0900

    -SpringMVC login-example
    1.HttpSession을 통해 로그인 기능 구현

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,12 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
     //@GetMapping("/")
     public String home() {
@@ -22,7 +27,7 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model){
 
         if(memberId == null){
@@ -38,6 +43,46 @@ public class HomeController {
             return "home";
         }
 
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
+    }
+
+    //@GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model){
+
+        //세션 관리자에 저장된 회원 정보 조회
+        Object member
+                = (Member)sessionManager.getSession(request);
+
+        //로그인
+        if(member == null){
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model){
+        //session에는 true, false가있다 true인경우 기존에 존재하면 반환 없으면 새로생성함
+        //false인 경우 기존에 존재하면 반환하지만 새로 추가하지는 않는다 이를 통해 메모리 낭비를 줄여야한다.
+        HttpSession session = request.getSession(false);
+
+        if(session == null){
+            return "home";
+        }
+
+        Object loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember == null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
         model.addAttribute("member", loginMember);
         return "loginHome";
 
